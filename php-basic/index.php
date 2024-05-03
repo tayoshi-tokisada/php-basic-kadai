65935f7f7c633b7110f84da3681814880b0fe0e6
tayoshi-tokisada <mail.by.blacksheep@gmail.com>
2024-05-03 16:21:24 +0900

    lesson ~kadai_005

---
+++ php-basic/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <title>PHP基礎編</title>
+  </head>
+
+  <body>
+    <p>この文章はHTMLで出力しています。</p>
+    <p>
+      <?php
+      echo "この文章はPHPで出力しています。"
+      ?>
+    </p>
+  </body>
+</html>
